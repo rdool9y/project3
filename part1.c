a20ea0feb729b43927a708444cd48d7bf679af76
Robert_Michael_Dooley <cs61c-ke@hive18.CS.Berkeley.EDU>
2013-10-24 02:26:21 -0700

    blocking with intrinsics kind of works, still very slow

--- part1.c
+++ part1.c
@@ -11,57 +11,56 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     int kern_cent_X = (KERNX - 1)/2;
     int kern_cent_Y = (KERNY - 1)/2;
 
-    int blocksize = 32;
+    int blocksize = 110;
     int x_block, y_block, x, y, i, j, k, r;
-
-    float get_kernel;
-    float *kernel_pointer = &get_kernel;
 
     // declare holder of 4 identical kernel elements
     __m128 kernel_element = _mm_setzero_ps();
 
     // declare holder of next 4 to multiply
     __m128 photo = _mm_setzero_ps();
-    float photos_as_floats[4];
+    float photos_as_floats[4];//__attribute__((aligned(16)));
 
-    // declare holder of results
-    float products_to_add[4];
-
-    // declare pointer to results holder (then cast into float array);
+    // declare results holder (and corresponding float array);
     __m128 result = _mm_setzero_ps();
-    float result_as_array[4];
+    float result_as_array[4];//__attribute__((aligned(16)));
 
 
-    for(i = -kern_cent_X; i <= kern_cent_X; i++)
-        for(j = -kern_cent_Y; j <= kern_cent_Y; j++)
+    for(i = -kern_cent_X; i <= kern_cent_X; i++) {
+        for(j = -kern_cent_Y; j <= kern_cent_Y; j++) {
             // fill kernel_element (all four floats are same kernel element)
-	    get_kernel = kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX];
-            kernel_element = _mm_load1_ps(kernel_pointer);
+  	    kernel_element = _mm_load1_ps(kernel+(kern_cent_X-i)+(kern_cent_Y-j)*KERNX);
 	    
-            for(x_block = 0; x_block <= data_size_X-blocksize ; x_block+=blocksize)
-                for(y_block = 0; y_block <= data_size_Y-blocksize ; y_block+=blocksize)
-
-                    for(x = 0; x < blocksize; x++)
-			
-                        for(y = 0; y < blocksize; y+=4)
+            for(x_block = 0; x_block <= data_size_X-blocksize ; x_block+=blocksize) {
+                for(y_block = 0; y_block <= data_size_Y-blocksize ; y_block+=blocksize) {
+                    for(x = 0; x < blocksize; x++) {
+                        for(y = 0; y < blocksize; y+=4) {
                             // fill photo_to_multiply USING if to set to 0.
 			    // possible optimization later if find out row vs. column - major?
+			    // Because can just zero-pad and load directly
 			    
-                            for(k = 0; k < 4; k++)
+                            for(k = 0; k < 4; k++) {
 			    
-	  	            if((x_block+x+i)>-1 && (x_block+x+i)<data_size_X && (y_block+y+j+k)>-1 && (y_block+y+j+k)<data_size_Y)
-                                photos_as_floats[k] = 0;
-                            else
-			        photos_as_floats[k] = in[(x_block+x+i) + (y_block+y+j+k)*data_size_X];
+	  	                if((x_block+x+i)>-1 && (x_block+x+i)<data_size_X && (y_block+y+j+k)>-1 && (y_block+y+j+k)<data_size_Y) {
+                                    photos_as_floats[k] = in[(x_block+x+i) + (y_block+y+j+k)*data_size_X]; }
+                                else {
+				    photos_as_floats[k] = 0; }
+			    }
 				
-			    // multiply and put results in output
-	                    photo = _mm_load_ps(photos_as_floats);	    
-	                    result = _mm_mul_ps (kernel_element, photo);
-			    _mm_store_ps(result_as_array, result);
+      			        // multiply and put results in output
+	                        photo = _mm_load_ps(photos_as_floats);	    
+	                        result = _mm_mul_ps (kernel_element, photo);
+			        _mm_store_ps(result_as_array, result);
                                
-			    for (r = 0; r < 4 ; r++)                            			    
-			        out[(x_block+x)+((y_block+y+r)*data_size_X)] += result_as_array[r];
-				                                    
+			        for (r = 0; r < 4 ; r++) {                            			    
+			            out[(x_block+x)+((y_block+y+r)*data_size_X)] += result_as_array[r]; }
+			    	
+			}
+                    }
+	        }		    
+	    }		    		    
+	}		    		    
+    }		    			                                    
 
 
     // leftover (not covered by blocking)
