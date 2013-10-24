35915b4788344d4fbb37a5915f43f59f7d298e8d
Chanoknan_Vorasucha <cs61c-ki@hive20.CS.Berkeley.EDU>
2013-10-24 08:46:36 -0700

    another convolution

--- part1.c
+++ part1.c
@@ -4,6 +4,36 @@
 #define KERNX 3 //this is the x-size of the kernel. It will always be odd.
 #define KERNY 3 //this is the y-size of the kernel. It will always be odd.
 
+int conv2D2(float* in, float* out, int data_size_X, int data_size_Y,
+                    float* kernel)
+{
+    // the x coordinate of the kernel's center
+    int kern_cent_X = (KERNX - 1)/2;
+    // the y coordinate of the kernel's center
+    int kern_cent_Y = (KERNY - 1)/2;
+    
+    // main convolution loop
+	for(int x = 0; x < data_size_X; x+=blocksize){ // the x coordinate of the output location we're focusing on
+		for(int y = 0; y < data_size_Y; y+=blocksize){ // the y coordinate of theoutput location we're focusing on
+			for (int a = x; a < x + blocksize; a++) {
+				for (int b = y; b < y + blocksize; b++) {
+					for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
+						for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
+							// only do the operation if not out of bounds
+							if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
+							//Note that the kernel is flipped
+								out[a+b*data_size_X] += 
+									kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(a+i) + (b+j)*data_size_X];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return 1;
+}
+
 
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                     float* kernel)
