adf55da34e91a6ddf8d86695005a59ac1c3f7379
Chanoknan_Vorasucha <cs61c-ki@hive1.CS.Berkeley.EDU>
2013-10-26 15:36:09 -0700

    unrolled. Still get an error-access mem incorrectly at some point that's not in tail handling section?(from OH)

--- part1.c
+++ part1.c
@@ -5,7 +5,7 @@
 #define KERNX 3 //this is the x-size of the kernel. It will always be odd.
 #define KERNY 3 //this is the y-size of the kernel. It will always be odd.
 
-/*
+
 int fah_conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 	   float* kernel)
 {
@@ -14,62 +14,104 @@ int fah_conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
 
-    int blocksize = 150;
-    int ker_x[4] = {KERNX,KERNX,KERNX,KERNX};
-    __m128i ker_x_size = __mm_loadu_si128((__m128i*)(ker_x));
-    int dt_size_x[4] = {data_size_X,data_size_X,data_size_X,data_size_X};
-    __m128i dt_x = __mm_loadu_si128((__m128i*)(dt_size_x));
+    int blocksize = 256; // must be multiple of 4 (or possibly 8/12/16? if loop unrolling)
 
+    // Declare Intrinsics Registers :
+    __m128 input_vector1 = _mm_setzero_ps();
+    __m128 output_vector1 = _mm_setzero_ps();
+    __m128 kernel_vector = _mm_setzero_ps();
+    __m128 product_vector1 = _mm_setzero_ps();
+
+    // Intrinsics Registers for unrolling:
+    __m128 input_vector2 = _mm_setzero_ps();
+    __m128 output_vector2 = _mm_setzero_ps();
+    __m128 product_vector2 = _mm_setzero_ps();
+    
+    // build zero-padded copy 
+    int padding_x = (KERNX / 2);  // can we assume that kernel is a square matrix??
+    int padding_y = (KERNY /2);
+    int padded_size = (data_size_X + 2*padding_x) * (data_size_Y + 2*padding_y); // not initialized to zero?
+    float* padded_in = malloc(padded_size * sizeof(float));
+    
+    int x,y;
+    
+    for(x = 0; x < padded_size; x++) {                                       // manually fill with zero
+        padded_in[x] = 0.0f;                                                 // optimize: zero edges only
+	} 
+   
+    for(y = 0; y < data_size_Y; y++) {
+        for(x = 0; x < data_size_X; x++) {
+	    padded_in[(x+padding_x) + (y+padding_y)*(data_size_X + 2*padding_y)] = in[x+y*data_size_X];
+	}
+	}      
+    /*
+    memset(&padded_in, 0.0f, (data_size_X+(2*padding_x))*(data_size_Y+(2*padding_y)));
+    for(y =0; y < data_size_Y; y++) {
+	memset(&padded_in, 0.0f, padding_x);
+	memcpy(&padded_in, &in, data_size_X);
+	memset(&padded_in, 0.0f, padding_x);
+    }
+    memset(&padded_in, 0.0f, (data_size_X+(2*padding_x))*(data_size_Y+(2*padding_y)));
+    */
+    int a, b, i, j;
+    
     // main convolution loop
-    for(int x = 0; x < data_size_X; x+=blocksize){ // the x coordinate of the output location we're focusing on
-	for(int y = 0; y < data_size_Y; y+=blocksize){ // the y coordinate of theoutput location we're focusing on
-	    for (int a = x; a < x + blocksize && a < data_size_X; a++) {
-		for (int b = y; b < y + blocksize && b < data_size_Y; b++) {
-		    for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
-			for(int j = -kern_cent_Y; j <= kern_cent_Y && a+i>-1 && a+i<data_size_X && b+j>-1 && b+j<data_size_Y; j++){ // kernel unflipped y coordinate
-			    // only do the operation if not out of bounds
-			    // int sum = 0;
-			       __m128i a_vec = _mm_setzero_sil123();
-			    if(a+i>-1 && a+i<data_size_X && b+j>-1 && b+j<data_size_Y){
-			    //
-			    //Note that the kernel is flipped
-			    for(int c = 0; c < 4; c++) {
-				__m128i ker_x_vec = __mm_setzero_si128();
-				ker_x_vec = __mm_loadu_si128((__m128i*)(kern_cent_X-i));
-				__m128i ker_y_vec = __mm_setzero_si128();
-				ker_y_vec = __mm_loadu_si128((__m128i*)(kern_cent_Y-j));
-				ker_y_vec = __mm_mul_ps(ker_y_vec, ker_x_size);
-				ker_x_vec = __mm_add_ps(ker_x_vec, ker_y_vec);
+    for(y = 0; y < data_size_Y; y+=blocksize){ 
+        for(x = 0; x < data_size_X; x+=blocksize){ 
+            for(b = y; b < y + blocksize && b < data_size_Y; b++) {        // no leftovers for y
+       	        for(a = x; a < x + blocksize && a < data_size_X; a+=8) {   // leftovers for x b/c increment by 4
+                    // set output vector to 0
+                    output_vector1 = _mm_setzero_ps();
+		    output_vector2 = _mm_setzero_ps();
+		    
+		    for(i = -kern_cent_X; i <= kern_cent_X; i++){          // inner loop; after all iterations, write 4 output sums
+		        for(j = -kern_cent_Y; j <= kern_cent_Y; j++){ 
 
-				__m128i in_a_i = __mm_setzero_si128();
-				in_a_i = __mm_loadu_si128((__m128i*)(a+i));
-				__m128i in_b_j = __mm_setzero_si128();
-				in_b_j = __mm_loadu_si128((__m128i*)(b+j));
-				in_b_j = __mm_mul_ps(in_b_j, dt_x);
-				in_a_i = __mm_add_ps(in_a_i, in_b_j);
-
-				int knl[4] = {0,0,0,0};
-				int input[4] = {0,0,0,0};
-				__mm_storeu_si128(knl, ker_x_vec);
-				__mm_storeu_si128(input, in_a_i);
-
-				__mm_storeu_si128( knl , ker_x_vec );
-				__mm_storeu_si128(input, in_a_i);
-
-				out[a+b*data_size_X] += knl[0];
-
-                                out[a+b*data_size_X] +=
-    			        kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(a+i) + (b+j)*data_size_X];
-			    }
+			    kernel_vector = _mm_load1_ps(kernel + ((kern_cent_X-i) + (kern_cent_Y-j)*KERNX));
+			    input_vector1 = _mm_loadu_ps(padded_in + ((a+i+padding_x) + (b+j+padding_y)*(data_size_X+2*padding_y)));
+			    // above must be loadu; can't use aligned load
+			    
+                            product_vector1 = _mm_mul_ps(kernel_vector1, input_vector1);
+                            output_vector1 = _mm_add_ps(output_vector1, product_vector1);
+			    
+			    input_vector2 = _mm_loadu_ps(padded_in + 4 + ((a+i+padding_x) + (b+j+padding_y)*(data_size_X+2*padding_y)));
+			    product_vector2 = _mm_mul_ps(kernel_vector, input_vector2);
+			    output_vector2 = _mm_add_ps(output_vector2, product_vector2);
+			    
 			}
-		    }
+                    }
+ 		    // After inner loop completes, write output vector to output matrix
+		    // must be storeu; can't use store aligned
+                    _mm_storeu_ps(out + (a + b*data_size_X), output_vector1);
+		    _mm_storeu_ps(out + 4 + (a + b*data_size_X), output_vector2);
 		}
 	    }
 	}
     }
+    
+    float output_float, kernel_float, input_float, product_float;
+    
+    // Deal with leftovers (0-3 columns on far right of out)
+    for(b = 0; b < data_size_Y; b++) {        
+        for(a = (data_size_X/4)*4; a < data_size_X; a++) {   
+            // set output to 0
+            output_float = 0.0f;
+		    
+ 	    for(i = -kern_cent_X; i <= kern_cent_X; i++){          // inner loop : all kernel elements
+	        for(j = -kern_cent_Y; j <= kern_cent_Y; j++){ 
+		    
+                    product_float = kernel[(kern_cent_X - i) + (kern_cent_Y-j)*KERNX] * padded_in[(a+i+padding_x)+(b+j+padding_y)*(data_size_X+2*padding_y)];
+                    output_float += product_float;
+                }
+            }
+                    out[a + b*data_size_X] = output_float;
+	}
+    }
+ 
+    free(padded_in);
+    
     return 1;
 }
-*/
 
 
 /* zero padding   : yes
