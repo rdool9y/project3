d8e61c072eeecafcff095ac48f97abafdf22dd1d
Robert_Michael_Dooley <cs61c-ke@hive26.CS.Berkeley.EDU>
2013-11-02 20:02:42 -0700

    Defined padding_x and padding_y as constants

--- part2.c
+++ part2.c
@@ -2,7 +2,8 @@
 #include <omp.h>
 #define KERNX 3 //this is the x-size of the kernel. It will always be odd.
 #define KERNY 3 //this is the y-size of the kernel. It will always be odd.
-
+#define padding_x 1
+#define padding_y 1
 
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
        float* kernel)
@@ -21,8 +22,8 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     __m128 input_vector4, output_vector4, product_vector4, input_vector5, output_vector5, product_vector5, input_vector6, output_vector6, product_vector6;
     __m128 input_vector7, output_vector7, product_vector7, input_vector8, output_vector8, product_vector8, input_vector9, output_vector9, product_vector9;
     
-    int padding_x = (KERNX / 2);  // can we assume that kernel is a square matrix??
-    int padding_y = (KERNY /2);
+//    int padding_x = (KERNX / 2);  // can we assume that kernel is a square matrix??
+//    int padding_y = (KERNY /2);
     int padded_size = (data_size_X + 2*padding_x) * (data_size_Y + 2*padding_y); // not initialized to zero?
     float* padded_in = malloc(padded_size * sizeof(float));
     
